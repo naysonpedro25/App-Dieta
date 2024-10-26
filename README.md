594c4378438577923d66bda4f42e4a77597caa11
nayson <naysonpedro2020@gmail.com>
2024-10-26 12:00:40 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,1 @@
+# App: Personal-Diet
---
+++ backend/src/infra/repository/UserRepository.ts
@@ -0,0 +1,33 @@
+import { PrismaClient, User} from "@prisma/client"
+import { getPrismaInstance } from "../../PrismaSingleton";
+
+export class UserRepository{
+    private db : PrismaClient = getPrismaInstance();
+    static userRepositorySingleton: UserRepository;
+    
+    static getUserRepository() {
+    if (!this.userRepositorySingleton)this.userRepositorySingleton = new UserRepository();
+        return this.userRepositorySingleton;
+    }
+    
+    async createUser(dataUser : User): Promise<User> {
+        return (await this.db.user.create({ data: dataUser }));
+        
+    }
+
+    async findUserByEmail(email: string): Promise<User | null >{
+        return ( await this.db.user.findUnique({where: {email: email}}));
+
+    }
+    async findUserById(id: number): Promise<User | null>{
+        return (await this.db.user.findUnique({where: {id: id}}));
+        
+    }
+
+    async getAllUsers() {
+        return  (await this.db.user.findMany());
+
+    }
+
+
+}
